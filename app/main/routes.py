6ba1f3065ca01e43ae314044e6fb39fc7ab8478e
Noudke <skynoudyo.fm@gmail.com>
2021-12-21 19:49:09 +0100

    Documentatie binnen de code. (main)

--- app/main/routes.py
+++ app/main/routes.py
@@ -39,24 +39,25 @@ def index():
 @login_required
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
-    posts = [
-        {'author': user, 'body': 'Test post #1'},
-        {'author': user, 'body': 'Test post #2'}
-    ]
     form = EmptyForm()
-    return render_template('user.html', user=user, posts=posts, form=form)
+    return render_template('user.html', user=user, form=form)
 
 
 @bp.route('/edit_profile', methods=['GET', 'POST'])
 @login_required
 def edit_profile():
+    # De Form voor het aanpassen van het profiel.
     form = EditProfileForm(current_user.username)
+
+    # Als de gebruiker aangeeft aanpassingen te willen maken aan het profiel.
     if form.validate_on_submit():
         current_user.username = form.username.data
         current_user.about_me = form.about_me.data
         db.session.commit()
+        # Een Flash/bericht binnen de applicatie welke aangeeft dat de aanpassingen zijn gemaakt.
         flash('Your changes have been saved.')
         return redirect(url_for('main.edit_profile'))
+    # Als (bij sommige gedeeltes) geen aanpassingen zijn gemaakt.
     elif request.method == 'GET':
         form.username.data = current_user.username
         form.about_me.data = current_user.about_me
@@ -64,6 +65,8 @@ def edit_profile():
                            form=form)
 
 
+# De volgfuncties zijn (nog) niet toegepast binnen de applicatie. Deze functie zou toestaan voor gebruikers om anderen
+# te volgen.
 @bp.route('/follow/<username>', methods=['POST'])
 @login_required
 def follow(username):
@@ -107,9 +110,10 @@ def unfollow(username):
 @bp.route('/current_studies', methods=['GET', 'POST'])
 @login_required
 def current_studies():
+    # Een lijst met alle bestaande studies waarbinnen de gebruiker betrokken is.
     studies = [study for study in Study.query.all() if current_user in study.linked_users]
 
-    return render_template("current_studies.html", title='New Study', studies=studies)
+    return render_template("current_studies.html", title='Current studies', studies=studies)
 
 
 @bp.route('/standard_questions/<username>', methods=['GET', 'POST'])
@@ -117,8 +121,10 @@ def current_studies():
 def standard_questions(username):
     user = User.query.filter_by(username=username).first_or_404()
     studies = Study.query.all()
+    # Alle kernvariabelen welke binnen de applicatie standaard horen of de gebruiker heeft aangemaakt.
     corevariables = [core_variable for core_variable in CoreVariable.query.filter_by(user_id=current_user.id)] + \
                     [core_variable for core_variable in CoreVariable.query.filter_by(user_id=None)]
+    # Een query met alle standaardvragen van de gebruiker.
     questions = StandardQuestion.query.filter_by(user_id=user.id)
 
     return render_template("standard_questions.html", title='New Study', user=user, studies=studies,
@@ -129,15 +135,16 @@ def standard_questions(username):
 @bp.route('/standard_questions/new_question_user/<name_corevariable>/<username>', methods=['GET', 'POST'])
 @login_required
 def new_standard_question(name_corevariable, username):
+    # De Form voor het aanmaken van een nieuwe standaardvraag.
     form = CreateNewQuestionUser()
 
+    # Als de gebruiker aangeeft een nieuwe standaardvraag aangemaakt te hebben.
     if form.validate_on_submit():
-        core_variable = CoreVariable.query.filter_by(name=name_corevariable).first_or_404()
+        corevariable = CoreVariable.query.filter_by(name=name_corevariable).first_or_404()
         user = User.query.filter_by(username=username).first_or_404()
-
         new_question = StandardQuestion(question=form.name_question.data,
                                         user_id=user.id,
-                                        corevariable_id=core_variable.id)
+                                        corevariable_id=corevariable.id)
         db.session.add(new_question)
         db.session.commit()
 
@@ -149,6 +156,7 @@ def new_standard_question(name_corevariable, username):
 @bp.route('/remove_standard_question/<id_question>', methods=['GET', 'POST'])
 @login_required
 def remove_standard_question(id_question):
+    # Het verwijderen van de standaardvraag.
     StandardQuestion.query.filter_by(id=id_question).delete()
     db.session.commit()
 
@@ -160,18 +168,20 @@ def remove_standard_question(id_question):
 def standard_demographics(username):
     user = User.query.filter_by(username=username).first_or_404()
     studies = Study.query.all()
+    # Alle standaarddemografieken welke de gebruiker heeft aangemaakt.
     demographics = [demographic for demographic in StandardDemographic.query.filter_by(user_id=current_user.id)]
-    questions = StandardQuestion.query.filter_by(user_id=user.id)
 
     return render_template("standard_demographics.html", title='Standard demographics', user=user, studies=studies,
-                           demographics=demographics, questions=questions)
+                           demographics=demographics)
 
 
 @bp.route('/standard_questions/new_question_user', methods=['GET', 'POST'])
 @login_required
 def new_standard_demographic():
+    # De Form voor het aanmaken van een nieuwe standaarddemografiek.
     form = CreateNewDemographicForm()
 
+    # Als de gebruiker aangeeft een nieuwe standaarddemografiek aangemaakt te hebben.
     if form.validate_on_submit():
 
         new_demographic = StandardDemographic(name=form.name_of_demographic.data,
@@ -192,9 +202,11 @@ def new_standard_demographic():
 @login_required
 def remove_standard_demographic(id_demographic):
     standard_question = StandardDemographic.query.filter_by(id=id_demographic).first()
+    # Als de standaardvraag niet van de huidige gebruiker is aangeven dat de gebruiker niet geauthoriseerd is.
     if standard_question.user_id != current_user.id:
         return redirect(url_for('main.not_authorized'))
 
+    # Het verwijderen van de standaarddemografiek.
     StandardDemographic.query.filter_by(id=id_demographic).delete()
     db.session.commit()
 
@@ -219,19 +231,23 @@ def invalid_session():
 
 @bp.route('/d/e/<study_code>', methods=['GET', 'POST'])
 def intro_questionlist(study_code):
+    # Als de gebruiker nog niet in een sessie zit een nieuwe sessie aanmaken.
     if "user" not in session:
         session["user"] = str(uuid.uuid4())
         session["study"] = study_code
 
+    # Als de gebruiker al in een sessie zit verwijzen naar de vragenlijst.
     if session["user"] in [case.session_id for case in Case.query.all()]:
         flash('You are currently already in a session. Complete the questionnaire.')  # return eerste blok vragenpagina
         return redirect(url_for('main.questionlist', study_code=study_code, questionlist_number=0))
 
+    # De Form om aangeven te starten met het onderzoek.
     form = GoToStartQuestionlist()
     study = Study.query.filter_by(code=study_code).first()
     questionnaire = Questionnaire.query.filter_by(study_id=study.id).first()
     demographics = [demographic for demographic in Demographic.query.filter_by(questionnaire_id=questionnaire.id)]
 
+    # Als de gebruiker aangeeft door te willen gaan naar de start van de vragenlijst.
     if form.validate_on_submit():
         return redirect(url_for('main.start_questionlist', study_code=study_code))
 
@@ -244,12 +260,15 @@ def start_questionlist(study_code):
     study = Study.query.filter_by(code=study_code).first()
     questionnaire = Questionnaire.query.filter_by(study_id=study.id).first()
 
+    # Een dictionary met de demografieken en een "return field" zodat de gebruiker alle demografieken kan invullen en
+    # deze ook daadwerkelijk opgeslagen worden.
     demographics_dict = {}
     demographics = [demographic for demographic in Demographic.query.filter_by(questionnaire_id=questionnaire.id)]
     for demographic in demographics:
         demographics_dict[demographic.name] = demographic.return_field()
     form = DynamicTestForm(demographics_dict)
 
+    # Als de gebruiker aangeeft de demografieken ingevuld te hebben.
     if form.validate_on_submit():
         if session["user"] in [case.session_id for case in Case.query.all()] and session["study"] == study_code:
             flash('You are currently already in a session. Complete the questionnaire.')
@@ -257,23 +276,29 @@ def start_questionlist(study_code):
 
         study = Study.query.filter_by(code=study_code).first()
         questionnaire = Questionnaire.query.filter_by(study_id=study.id).first()
+        # Het toevoegen van een case aan de database.
         case = Case(session_id=session["user"], questionnaire_id=questionnaire.id)
         db.session.add(case)
         db.session.commit()
 
+        # De antwoorden op de demografieken worden nog niet opgeslagen in de database, maar wel in de sessie.
         session["demographic_answers"] = []
         for (demographic, answer) in zip([demographic for demographic in demographics], form.data.values()):
             demographic_answer = DemographicAnswer(answer=answer, demographic_id=demographic.id,
                                                    case_id=Case.query.filter_by(session_id=session["user"]).first().id)
             session["demographic_answers"].append(demographic_answer)
 
+        # Een dictionary met een numerieke key (0 tot en met zoveel) en de vragengroep (questiongroup_dict).
         questiongroup_dict = {}
-        questiongroups = [questiongroup for questiongroup in
-                          QuestionGroup.query.filter_by(questionnaire_id=questionnaire.id)]
+        questiongroups = [questiongroup for questiongroup in questionnaire.linked_questiongroups]
         keys = range(len(questiongroups))
         for i in keys:
             questiongroup_dict[i] = questiongroups[i]
 
+        # Evenals de demografische antwoorden worden de antwoorden op de vragen nog niet opgeslagen in de database, maar
+        # wel in de sessie. De eerste twee sessiedata hieronder zijn om de vragenlijst goed te renderen (de eerste om
+        # de vragenlijst op te delen tussen de vragengroepen, de tweede om te gaan naar het eindscherm zodra de laatste
+        # is ingevuld).
         session["questionlist_questiongroups"] = questiongroup_dict
         session["questionlist_maxamount"] = QuestionGroup.query.filter_by(questionnaire_id=questionnaire.id).count()
         session["answers"] = []
@@ -285,33 +310,45 @@ def start_questionlist(study_code):
 
 @bp.route('/c/e/<study_code>/<questionlist_number>', methods=['GET', 'POST'])
 def questionlist(study_code, questionlist_number):
+    # Als de vragengroepnummer groter is dan de hoeveelheid vragengroepen wordt de gebruiker verwezen naar het einde.
     if int(questionlist_number) >= session['questionlist_maxamount']:
         return redirect(url_for('main.ending_questionlist', study_code=study_code))
 
     study = Study.query.filter_by(code=study_code).first()
     questionnaire = Questionnaire.query.filter_by(study_id=study.id).first()
-    questions_dict = {}
     questionlist = session["questionlist_questiongroups"][int(questionlist_number)]
     questions = [question for question in Question.query.filter_by(questiongroup_id=questionlist.id)]
 
+    # Een dictionary met de vraag als key en een bijbehorende Field om in te vullen als waarde (questions_dict).
+    questions_dict = {}
     for question in questions:
         questions_dict[question.question] = RadioField(question.question,
                                                        choices=[number for number in range(1, questionnaire.scale + 1)])
     form = DynamicTestForm(questions_dict)
 
+    # De volgende vragengroepnummer voor de verwijzing zodra de gebruiker dit gedeelte van de vragenlijst heeft ingevuld
     next_questionlist_number = int(questionlist_number) + 1
 
+    # Als de gebruiker aangeeft dit gedeelte van de vragenlijst ingevuld te hebben.
     if form.validate_on_submit():
         for (question, value) in zip([question for question in questions], form.data.values()):
+            # Deze "isinstance" regel wordt opgeroepen om enkele waarden welke toegevoegd werden en niet binnen de
+            # antwoorden horen weg te werken. Dit kan gezien worden als een makkelijke work-around.
             if isinstance(value, str) and len(value) < 4:
+                # Als een antwoord op de vraag al gegeven is binnen de sessie.
                 if question.id in [answer.question_id for answer in session["answers"]]:
                     for answer in session["answers"]:
+                        # Voor de vraag die inderdaad al beantwoord is.
                         if answer.question_id == question.id:
+                            # Als de vraag met "reversed_score" werkt de gegeven score omdraaien.
                             if question.reversed_score:
                                 answer.score = reverse_value(value, questionnaire.scale)
                             else:
                                 answer.score = value
+                # Als nog geen antwoord op de vraag gegeven is binnen de sessie.
                 else:
+                    # Als de vraag met "reversed_score" werkt de gegeven score omdraaien. Het antwoord sowieso toevoegen
+                    # aan de sessie.
                     if question.reversed_score:
                         answer = Answer(score=reverse_value(value, questionnaire.scale), question_id=question.id,
                                         case_id=Case.query.filter_by(session_id=session["user"]).first().id)
@@ -320,6 +357,7 @@ def questionlist(study_code, questionlist_number):
                         answer = Answer(score=value, question_id=question.id,
                                         case_id=Case.query.filter_by(session_id=session["user"]).first().id)
                         session["answers"].append(answer)
+        # Naar het volgende onderdeel van de vragenlijst gaan.
         return redirect(
             url_for('main.questionlist', study_code=study_code, questionlist_number=next_questionlist_number))
 
@@ -331,24 +369,30 @@ def questionlist(study_code, questionlist_number):
 
 @bp.route('/g/e/<study_code>', methods=['GET', 'POST'])
 def ending_questionlist(study_code):
-    # Voor het geval de gebruiker probeert naar het einde te gaan zonder dat alle vragen zijn beantwoord.
     study = Study.query.filter_by(code=study_code).first()
     questionnaire = Questionnaire.query.filter_by(study_id=study.id).first()
     total_question_number = 0
     for questiongroup in QuestionGroup.query.filter_by(questionnaire_id=questionnaire.id):
         total_question_number += Question.query.filter_by(questiongroup_id=questiongroup.id).count()
+    # Voor het geval de gebruiker probeert naar het einde te gaan zonder dat alle vragen zijn beantwoord.
     if len(session["answers"]) < total_question_number:
         flash('You have not answered all of the questions yet. Finish the questions.')
         return redirect(url_for('main.questionlist', study_code=study_code, questionlist_number=0))
 
+    # De Form voor het aangeven dat de gebruiker inderdaad klaar is met de vragenlijst.
     form = EmptyForm()
+
+    # Als de gebruiker aangeeft klaar te zijn met de vragenlijst.
     if form.validate_on_submit():
+        # Het opslaan van de antwoorden op de vragen in de database.
         for answer in session["answers"]:
             db.session.add(answer)
             db.session.commit()
+        # Het opslaan van de demografische antwoorden in de database.
         for answer in session["demographic_answers"]:
             db.session.add(answer)
             db.session.commit()
+        # Aangeven dat de vragenlijst voltooid is door de gebruiker/specifieke case.
         Case.query.filter_by(session_id=session["user"]).first().completed = True
         db.session.commit()
         session.clear()
